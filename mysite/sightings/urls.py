7a3536ca7573f6fb00c1a1082230fe0579b1acc2
XIAOPENG LI <JerryLee@lixiaopengdeMBP.lan1>
2019-12-08 13:47:04 -0500

    modified urls.files and map.html

--- mysite/sightings/urls.py
+++ mysite/sightings/urls.py
@@ -4,7 +4,7 @@ app_name = 'sightings'
 
 urlpatterns = [
 	path('', views.list, name = 'list'),
-        path('<str:unique_id>/', views.update, name='update'),
 	path('add/', views.add, name = 'add'),
 	path('stats/', views.stats, name = 'stats'),
+        path('<str:unique_id>/', views.update, name='update'),
 	] 
