a7d3bb5f807e55bdd8aa711522dc6df704a6bf76
XIAOPENG LI <JerryLee@lixiaopengdeMBP.lan1>
2019-12-07 22:34:17 -0500

    modified html files and views.py file!

--- mysite/sightings/views.py
+++ mysite/sightings/views.py
@@ -1,38 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from .models import Squirrel
-
+from .forms import getFormforSightings
 from django.views.generic.edit import CreateView
 from django.urls import reverse, reverse_lazy
 
 def list(request, *args, **kwargs):
-    list = Squirrel.objects.all()
+    list_ = Squirrel.objects.all()
     fields = ['Unique_Squirrel_ID', 'Date']
-    context = {'Squirrels': list, 'fields': fields, }
+    context = {'squirrels': list_, 'fields': fields, }
     return render(request, 'sightings/list.html', context)
 
 def update(request, unique_id):
     obj = get_object_or_404(Squirrel, Unique_Squirrel_ID = unique_id)
     form = getFormforSightings(request.POST or None, instance = obj) 
-    if form.is_valid()
+    if form.is_valid():
         form.save()
         return redirect(f'/sightings/{unique_id}')
     context ={
-        'form':form
+        'form':form,
         'squirrel_id':unique_id,
     }
     return render(request, 'sightings/update.html', context)
 
-class add(CreateView):
-    model = Squirrel
-    fields = '__all__'
-    success_url = reverse_lazy('sightings:sightings')
+def add(request):
+    if request.method == 'POST':
+        form = getFormforSightings(request.POST)
+        if form.is_valid():
+            a = form['Unique_Squirrel_ID'].value()
+            sighting = form.save(commit=False)
+            sighting.save()
+            return redirect(f'/sightings/{a}')
+    else:
+        form = getFormforSightings()
+    context ={
+        'form':form,
+    }
+    return render(request, 'sightings/add.html', context)
     
 
 def stats(request):
-    list = Squirrel.objects.all()
+    list1 = Squirrel.objects.all()
     context = {
-        'Squirrels': list 
+        'squirrels': list1 
     }
     return render(request, 'sightings/stats.html', context)
 
